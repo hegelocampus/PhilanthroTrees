de3edcdb85d0d759a7651bc34ba60a147a8311a7
Kaneka <kane.chhak@gmail.com>
2019-10-22 09:34:49 -0700

    Create validation for project

---
+++ validation/project.js
@@ -0,0 +1,32 @@
+const Validator = require('validator');
+const validText = require('./valid-text');
+
+module.exports = function validateProject(data) {
+  let errors = {};
+
+  data.name = validText(data.name) ? data.name : "";
+  data.description = validText(data.description) ? data.description : "";
+
+
+  if (!Validator.isLength(data.name, { min: 5, max: 30 })) {
+    errors.name = "Project name must be between 5 and 30 characters";
+  }
+
+  if (Validator.isEmpty(data.name)) {
+    errors.name = "Project name is required";
+  }
+
+  if (!Validator.isLength(data.description, { max: 250 })) {
+    errors.description = "Project description cannot exceed 250 characters";
+  }
+
+  if (Validator.isEmpty(data.email)) {
+    errors.email = "Email field is required";
+  }
+
+  return {
+    errors,
+    isValid: Object.keys(errors).length === 0
+  }
+
+}
