9e56856279c2f5b0a5cbb6a230a6f580163c6ec3
Garvin <gming27@gmail.com>
2019-10-23 12:36:34 -0400

    creates projectReducer with RECEIVE_PROJECT case

---
+++ frontend/src/reducers/projects_reducer.js
@@ -0,0 +1,22 @@
+import merge from 'lodash/merge';
+
+import { RECEIVE_PROJECT, RECEIVE_PROJECT_ERRORS } from '../actions/project_actions';
+
+
+const projectReducer = (state ={}, action) =>{
+  Object.freeze(state);
+  let newState;
+
+  switch(action.type){
+    
+    case RECEIVE_PROJECT:
+      let project = {[action.project.id]: action.project}
+      newState = merge({}, state, project);
+      return newState;
+
+    default:
+      return state;
+  }
+}
+
+export default projectReducer;
