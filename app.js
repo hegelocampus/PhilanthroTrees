192a2cb7748c0d1a4fc1a991773a70871ce4d9f9
Bee <pizzaman8099@gmail.com>
2019-10-23 08:34:30 -0700

    Add changes to package.json and app.js required to deploy to heroku

--- app.js
+++ app.js
@@ -63,6 +63,12 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('frontend/build'));
+  app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  })
+}
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
