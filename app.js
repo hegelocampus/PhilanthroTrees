87408e4e6154b1c09f5f8973b5060ca859e98a65
Bee <pizzaman8099@gmail.com>
2019-10-23 09:28:43 -0700

    Move port handling to bin/www

--- app.js
+++ app.js
@@ -72,9 +72,6 @@ if (process.env.NODE_ENV === 'production') {
     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
   })
 }
-const port = process.env.PORT || 5000;
-
-app.listen(port, () => console.log(`Server is running on port ${port}`));
 
 module.exports = app;
 
