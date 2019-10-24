9ecddb8d33657c8c2ca6ca94c5f8aa5ab613f90b
Garvin <gming27@gmail.com>
2019-10-24 15:32:22 -0400

    adds createTask to the task_actions

--- frontend/src/actions/task_actions.js
+++ frontend/src/actions/task_actions.js
@@ -1,6 +1,7 @@
 import * as TaskApiUtil from '../util/task_api_util';
 
 export const RECEIVE_ALL_TASKS = 'RECEIVE_ALL_TASKS';
+export const RECEIVE_TASK = 'RECEIVE_TASK'; 
 export const RECEIVE_TASK_ERRORS = 'RECEIVE_TASK_ERRORS';
 
 export const fetchTasks = projectId => dispatch => (
@@ -10,6 +11,21 @@ export const fetchTasks = projectId => dispatch => (
     errors => dispatch(receiveErrors(errors.data))
   )
 );
+
+export const createTask = (projectId, task) => dispatch => (
+  TaskApiUtil.createTask(projectId, task)
+  .then(
+    task => dispatch(receiveTask(task.data)),
+    errors => dispatch(receiveErrors(errors.data))
+  )
+)
+
+export const receiveTask = task => {
+  return({
+    type: RECEIVE_TASK,
+    task
+  })
+}
 
 export const receiveTasks = tasks =>{
   return({
