a563b2161a4a6d5741c7ababfd1b1ea071bfd9ad
Garvin <gming27@gmail.com>
2019-10-24 13:19:20 -0400

    creates CreateTask form

---
+++ frontend/src/components/project/create_task.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+
+class CreateTask extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      title: "",
+      details: "",
+    }
+    this.submit = this.submit.bind(this);
+  }
+
+  submit(e) {
+    e.preventDefault();
+    this.props.updateProject(this.state);
+  }
+
+  update(field) {
+    return (e) => {
+      this.setState({ [field]: e.target.value })
+    }
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.submit}>
+
+        <label>
+        Task Title
+        <input className="title" type="text"
+          value={this.state.title} onChange={this.update('title')} />
+        </label>
+
+        <label>
+        Task Description
+        <input className="details" type="text"
+          value={this.state.details} onChange={this.update('details')} />
+        </label>
+
+      <button type="submit">Create Task!</button>
+
+      </form>
+    )
+  }
+}
+
+export default CreateTask;
