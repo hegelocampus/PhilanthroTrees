0527ad737b87e2909ae6e907ca3f25d3b8801ac3
Garvin <gming27@gmail.com>
2019-10-24 15:35:50 -0400

    adds RECEIVE_TASK to tasks_reducer

--- frontend/src/reducers/tasks_reducer.js
+++ frontend/src/reducers/tasks_reducer.js
@@ -1,6 +1,6 @@
 import merge from 'lodash/merge';
 
-import { RECEIVE_ALL_TASKS } from '../actions/task_actions';
+import { RECEIVE_ALL_TASKS, RECEIVE_TASK } from '../actions/task_actions';
 
 const tasksReducer = (state= {}, action) => {
   Object.freeze(state);
@@ -13,6 +13,11 @@ const tasksReducer = (state= {}, action) => {
       Object.values(action.tasks).forEach(task =>{
         newState[task._id]= task
       });
+      return newState;
+    
+    case RECEIVE_TASK:
+      let task = { [action.task._id]: action.task }
+      newState = merge({}, state, task);
       return newState;
 
     default:
