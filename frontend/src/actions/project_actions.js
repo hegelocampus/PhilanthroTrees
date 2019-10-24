6f7f0923c858d782891317005a127310b4354f37
Garvin <gming27@gmail.com>
2019-10-24 17:19:57 -0400

    creates fetchProjects action in project_actions

--- frontend/src/actions/project_actions.js
+++ frontend/src/actions/project_actions.js
@@ -1,7 +1,23 @@
 import * as ProjectApiUtil from '../util/project_api_util';
 
 export const RECEIVE_PROJECT = 'RECEIVE_PROJECT';
+export const RECEIVE_ALL_PROJECTS = 'RECEIVE_ALL_PROJECTS';
 export const RECEIVE_PROJECT_ERRORS = 'RECEIVE_PROJECT_ERRORS';
+
+export const fetchProjects = communityId => dispatch =>(
+  ProjectApiUtil.fetchProjects(communityId)
+  .then(
+    projects => dispatch(receiveProjects(projects)),
+    errors => dispatch(receiveErrors(errors.data))
+  )
+)
+
+export const receiveProjects = projects => {
+  return({
+    type: RECEIVE_ALL_PROJECTS,
+    projects 
+  })
+}
 
 export const fetchProject = project => dispatch => (
   ProjectApiUtil.fetchProject(project)
