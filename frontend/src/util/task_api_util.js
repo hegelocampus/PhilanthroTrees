8e428991b4809aa2fd108c9f79d57debfdc640f9
Garvin <gming27@gmail.com>
2019-10-24 14:04:18 -0400

    creates createTask util method

--- frontend/src/util/task_api_util.js
+++ frontend/src/util/task_api_util.js
@@ -3,3 +3,7 @@ import axios from 'axios';
 export const fetchTasks = projectId => {
   return axios.get(`/api/projects/${projectId}/tasks`);
 }
+
+export const createTask = (projectId, task) => {
+  return axios.get(`/api/projects/${projectId}/tasks/create`, task)
+}
