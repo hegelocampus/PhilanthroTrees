01c9018dd7c4bd729e0cc88ee9a7eaf6e2f88ce7
Garvin <gming27@gmail.com>
2019-10-25 14:17:36 -0400

    creates updateTask api_util

--- frontend/src/util/task_api_util.js
+++ frontend/src/util/task_api_util.js
@@ -7,3 +7,7 @@ export const fetchTasks = projectId => {
 export const createTask = (projectId, task) => {
   return axios.post(`/api/projects/${projectId}/tasks/create`, task)
 }
+
+export const updateTask = (taskId, task) =>{
+  return axios.patch(`/api/tasks/${taskId}`, task)
+}
