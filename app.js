8ab6d540607cc58696be7834b64460a6f2d80d19
Bee <pizzaman8099@gmail.com>
2019-10-25 16:46:48 -0700

    Add nec changes to app and index to get sass to work

--- app.js
+++ app.js
@@ -31,14 +31,16 @@ app.use(sassMiddleware({
   dest: path.join(__dirname + '/frontend/public'),
   prefix: '%PUBLIC_URL%',
   debug: true,
-  outputStyle: 'compressed'
+  outputStyle: 'compressed',
+  response: false,
+  force: true
 }));
 
 // view engine setup
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('frontend/build'));
   app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'));
   })
 } else {
   //app.set('views', path.join(__dirname, 'views'));
@@ -46,7 +48,7 @@ if (process.env.NODE_ENV === 'production') {
   //app.use(express.static(path.join(__dirname, 'public')));
   app.use(express.static(path.join(__dirname, 'frontend/public')));
   app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'frontend', 'pubilc', 'index.html'));
+    res.sendFile(path.join(__dirname, 'frontend', 'pubilc', 'index.html'));
   })
 }
 
