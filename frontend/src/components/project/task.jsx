a8cc8727b6daad14a3b9dbc20494c5703f23b64f
Garvin <gming27@gmail.com>
2019-10-25 14:09:12 -0400

    converts Task list elements to a form and passes updateTask dispatch to Task Component

--- frontend/src/components/project/task.jsx
+++ frontend/src/components/project/task.jsx
@@ -1,12 +1,49 @@
 import React from 'react'
 
 class Task extends React.Component{
+  constructor(props){
+    super(props);
+
+    this.state = {
+      completed: false
+    }
+  }
+
+  submitTask(){
+    e.preventDefault();
+    this.props.updateTask(this.state);
+  }
+
+  update(field) {
+    return (e) => {
+      // e.preventDefault();
+      let set = this.state[field] ? false : true;
+      this.setState({
+        title: this.props.task.title,
+        details: this.props.task.details,
+        [field]: set 
+        });
+    }
+  }
+
+
   render(){
 
     return(
       <li key={this.props.task._id}>
-        <h4 id="task-title">{this.props.task.title}</h4>
-        <p id="task-details">{this.props.task.details}</p>
+        <form onSubmit={this.submitTask}>
+
+          <h4 id="task-title">{this.props.task.title}</h4>
+          <p id="task-details">{this.props.task.details}</p>
+
+          <label>
+            <input type="radio" value="Completed" onClick = {this.update("completed")} />
+            Completed
+          </label>
+
+          <input type="submit"  value="Done!"/>
+
+        </form>
       </li>
     )
   }
