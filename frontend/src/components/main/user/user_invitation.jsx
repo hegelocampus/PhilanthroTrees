acb79077fb0b855c815e8853fd12285c178223df
Garvin <gming27@gmail.com>
2019-10-26 14:37:10 -0400

    creates Invite Form Route on Community main

--- frontend/src/components/main/user/user_invitation.jsx
+++ frontend/src/components/main/user/user_invitation.jsx
@@ -3,24 +3,62 @@ import {
   useDispatch,
   useSelector
 } from 'react-redux';
-import { requestUser } from '../../../actions/user_actions';
+import { userInvite } from '../../../actions/user_actions';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
 
 export default (props) => {
   const dispatch = useDispatch();
 
   const currentUserId = useSelector(state => state.session.user.id);
   const currentUser = useSelector(state => state.entities.users[currentUserId]);
+  const community = useSelector(state =>{
 
-  useEffect(() => {
-    dispatch(updateInvite(userEmail));
-  },
-    //May have to include something else here to make it so user updates after task
-    //completeion, perhaps a subscription
-    [currentUserId, dispatch]
+    if (state.entities.community){
+      return({
+        id: state.entities.community.id,
+        name: state.entities.community.name
+      })
+    }
+  });
+
+  
+  return (
+    <React.Fragment>
+      <Formik
+        initialValues={{
+          emailAddress: '',
+          id: community.id,
+          name: community.name
+        }}
+        validate={values => {
+          let errors = {};
+          if (!values.emailAddress) {
+            errors.emailAddress = 'Required';
+          } else if (
+            !/^[A-Z0-9._%+-]/i.test(values.emailAddress)
+          ) {
+            errors.emailAddress = 'Invalid email';
+          }
+          return errors;
+        }}
+        onSubmit={(values, { setSubmitting }) => {
+          dispatch(userInvite(values.emailAddress, values)).then(
+            () => {
+              setSubmitting(false);
+            }
+          );
+        }}
+        render={({ errors, status, touched, isSubmitting }) => (
+          <Form>
+            <Field type="emailAddress" name="emailAddress" />
+            <ErrorMessage name="emailAddress" component="div" />
+            <button type="submit" disabled={isSubmitting}>
+              Send Invite
+            </button>
+          </Form>
+        )}
+      />
+    </React.Fragment>
   );
-
-  return(
-
-  )
 
 }
