522fc826bf8d95fb1d4b375ed392458c0f8cf230
Bee <pizzaman8099@gmail.com>
2019-10-28 09:04:38 -0700

    Minor cleanup

--- frontend/src/components/todo/todo_list.jsx
+++ frontend/src/components/todo/todo_list.jsx
@@ -46,7 +46,7 @@ class TodoList extends React.Component{
             <Link to={`/projects/${project._id}`}>
             {project.name}
             </Link>
-          </li> 
+          </li>
         )
       })
     }
