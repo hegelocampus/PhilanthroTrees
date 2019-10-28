fd7a7be5a6507a83148ab1395980af9bd9c64f26
Garvin <gming27@gmail.com>
2019-10-28 12:07:20 -0400

    properly nests the Todo List at the user home page

--- frontend/src/components/main/main_display.jsx
+++ frontend/src/components/main/main_display.jsx
@@ -9,7 +9,7 @@ import TopBar from './top_bar/top_bar';
 import UserDetail from './user/user_detail';
 import ProjectContainer from '../project/project_container';
 import Community from './community/community';
-import TodoListContainer from '../todo/todo_list_container';
+import TodoList from '../todo/todo_list_container';
 
 
 export default (props) => {
@@ -19,6 +19,7 @@ export default (props) => {
       <div className="main-container">
         {/* User Profile is always displayed at the top, right under the nav bar */}
         <UserDetail />
+        <TodoList />
         <Switch>
           <Route path={'/user/:userId'}>
             <h1>User show page</h1>
@@ -29,7 +30,6 @@ export default (props) => {
           />
           <Route path="/projects/:projectId"
           component={ProjectContainer} />
-          <Route path="/" component={ TodoListContainer } />
         </Switch>
       </div>
     </React.Fragment>
