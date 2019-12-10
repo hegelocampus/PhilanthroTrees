6f045520110929a29195ec58fc22e9fdd2dd7b31
Kaneka <kane.chhak@gmail.com>
2019-12-10 15:18:55 -0800

    Remove redundancy from alt text

--- frontend/src/components/splash/splash.jsx
+++ frontend/src/components/splash/splash.jsx
@@ -24,7 +24,7 @@ export default (props) => {
             Join members of your community today to make a better world both online and offline.
           </p>
           <div className="plant-sprites">
-            <img src={cactus1} alt="pixel cactus image"></img> <img src={cactus2} alt="pixel cactus image"></img> <img src={cactus1} alt="pixel cactus image"></img>
+            <img src={cactus1} alt="pixel cactus"></img> <img src={cactus2} alt="pixel cactus"></img> <img src={cactus1} alt="pixel cactus"></img>
           </div>
         </div>
 
@@ -41,7 +41,7 @@ export default (props) => {
            consequences.
         </p>
         <div className="plant-sprites">
-          <img src={violet} alt="pixel violet image"></img> <img src={rose} alt="pixel rose image"></img> <img src={violet} alt="pixel violet image"></img>
+          <img src={violet} alt="pixel violet"></img> <img src={rose} alt="pixel rose"></img> <img src={violet} alt="pixel violet"></img>
         </div>
       </div>
 
