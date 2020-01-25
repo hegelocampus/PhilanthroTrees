72d817e50d9f8f27874c1176f6e62d4dee28bc8a
Bee <pizzaman8099@gmail.com>
2020-01-25 14:32:51 -0800

    Clean up rendering of form

--- frontend/src/components/splash/session/session_form.js
+++ frontend/src/components/splash/session/session_form.js
@@ -23,65 +23,64 @@ export default ({ formType }) => {
           password2: ""
         }}
         onSubmit={values => dispatch(process(values))}
-        render={({ errors, status, touched }) => (
-          <Form className="session-form">
-            {formType === 'signup' && (
-              <React.Fragment>
-                <p> Username must be between 2 and 30 characters. Choose wisely! Other members of your community will be able to see </p>
-                <Field
-                  name='username'
-                  placeholder='Username'
-                  type='text'
-                />
-              </React.Fragment>
-            )}
-            <Field
-              name='email'
-              placeholder='E-mail'
-              type='email'
-            />
-            <Field
-              name='password'
-              placeholder='Password'
-              type='password'
-              required
-            />
-            {formType === 'signup' ? (
-              <>
-                <Field
-                  name='password2'
-                  placeholder='Confirm Password'
-                  type='password'
-                />
-                <button
-                  type='submit'
-                  className="big-button"
-                >
-                  Create Account
-                </button>
-              </>
-            ):(
-              <>
-                <button
-                  type='submit'
-                  className="big-button"
-                >
-                  Sign In
-                </button>
-                <button
-                  type="button"
-                  onClick={(e) => {
-                    dispatch(login(GUEST_LOGIN))
-                  }}
-                  className="big-button"
-                >
-                  Guest Login
-                </button>
-              </>
-            )}
-          </Form>
-        )}
-      />
+      >
+        <Form className="session-form">
+          {formType === 'signup' && (
+            <React.Fragment>
+              <p> Username must be between 2 and 30 characters. Choose wisely! Other members of your community will be able to see </p>
+              <Field
+                name='username'
+                placeholder='Username'
+                type='text'
+              />
+            </React.Fragment>
+          )}
+          <Field
+            name='email'
+            placeholder='E-mail'
+            type='email'
+          />
+          <Field
+            name='password'
+            placeholder='Password'
+            type='password'
+            required
+          />
+          {formType === 'signup' ? (
+            <>
+              <Field
+                name='password2'
+                placeholder='Confirm Password'
+                type='password'
+              />
+              <button
+                type='submit'
+                className="big-button"
+              >
+                Create Account
+              </button>
+            </>
+          ):(
+            <>
+              <button
+                type='submit'
+                className="big-button"
+              >
+                Sign In
+              </button>
+              <button
+                type="button"
+                onClick={(e) => {
+                  dispatch(login(GUEST_LOGIN))
+                }}
+                className="big-button"
+              >
+                Guest Login
+              </button>
+            </>
+          )}
+        </Form>
+      </Formik>
     </React.Fragment>
   );
 }
