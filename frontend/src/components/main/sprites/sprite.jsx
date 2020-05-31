37c4d53a24622a29067f1045e31d396fb1df9695
Bee Ellis <44533466+hegelocampus@users.noreply.github.com>
2020-05-31 12:44:54 -0700

    Refactor to avoid mutating level
    This also increases the clarity of the `img`'s alt tag. Before it would say ...at level 7 for all sprites before the community reached level 7, which is kind of confusing for the user.

--- frontend/src/components/main/sprites/sprite.jsx
+++ frontend/src/components/main/sprites/sprite.jsx
@@ -5,10 +5,10 @@ import '../../../stylesheets/sprite.scss';
 export default ({ level = 7, type = 'sprout' }) => {
   // Level 7 is around the level where the sprites actualy start to look like plants
   // so we should never use any sprite under level 7.
-  level = Math.max(level, 7);
+  const validLevel = Math.max(level, 7);
 
   let sprite;
-  for (let i = level; i > 0; i--){
+  for (let i = validLevel; i > 0; i--){
     if (sprite) {
       break
     }
